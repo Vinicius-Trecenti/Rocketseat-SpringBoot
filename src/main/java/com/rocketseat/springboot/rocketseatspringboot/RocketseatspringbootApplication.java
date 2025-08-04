75896d5a67874b1ed8f68efe6b38d67e218d9afa
Vinicius Trecenti <vinitrecenti@gmail.com>
2025-08-04 14:05:26 -0300

    Inicializando o Spring Boot

---
+++ src/main/java/com/rocketseat/springboot/rocketseatspringboot/RocketseatspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.springboot.rocketseatspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketseatspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketseatspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/springboot/rocketseatspringboot/RocketseatspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.springboot.rocketseatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketseatspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
